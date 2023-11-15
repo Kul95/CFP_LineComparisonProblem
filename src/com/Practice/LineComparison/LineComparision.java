b1414ee6b48fdc9165498c7dde47dc06004a5f09
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-11-16 01:13:51 +0530

    Added: UC2

--- src/com/Practice/LineComparison/LineComparision.java
+++ src/com/Practice/LineComparison/LineComparision.java
@@ -3,6 +3,7 @@ package com.Practice.LineComparison;
 public class LineComparision {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Problems...");
+
         double length1=lengthMethod(5,30,4,20);
         String str1=String.valueOf(length1);
         System.out.println(str1);
@@ -10,6 +11,10 @@ public class LineComparision {
         double length2=lengthMethod(1,50,4,20);
         String str2=String.valueOf(length2);
         System.out.println(str2);
+
+        //equals method
+        System.out.println("Equals method: "+str1.equals(str2));
+        System.out.println();
     }
     public  static  double lengthMethod(int x1,int x2,int y1,int y2){
         double length3=Math.sqrt(((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
